a5ff32f2151d3162c7ecee00d40288129f3442e1
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-20 10:28:39 -0300

    Create route "NotFound"
    Shown when making a request returns a 404 error

---
+++ app/assets/javascripts/leaf/views/errors/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-not-found">
+      <h1 class="title text-center mx-auto">Oops! That page doesn’t exist.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound',
+    metaInfo: {
+        title: 'Not Found'
+    }
+}
+</script>
+
+<style>
+
+</style>
