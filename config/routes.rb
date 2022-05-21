db7c59900f1a200ffc01969e8801d1d40d298e01
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-21 14:22:09 -0300

    Add the routes for webmanifest and dashboard json

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get 'leaf_spa/app'
   # The "root" route, and any other no-defined url, will be handled by the SPA
   root :to => "leaf_web#app", as: 'leaf'
+
+  get "manifest.webmanifest" => "metadata#webmanifest", as: :manifest
+  get "manifest.json" => "metadata#webmanifest"
+  get "dashboard.json" => "dashboard#index"
+  get "dashboard/uptime.json" => "dashboard#uptime"
+
   match '*path', to: 'leaf_web#app', via: :all
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
