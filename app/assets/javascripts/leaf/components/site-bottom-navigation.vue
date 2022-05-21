badf5ff78e224ac07c44aeee636e13807fa3ebd8
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2022-05-21 14:16:42 -0300

    Create the BottomNavigation (Mobile) Component

---
+++ app/assets/javascripts/leaf/components/site-bottom-navigation.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-bottom-navigation
+    v-if="isMobile"
+    background-color="#fff"
+    color="#4527a0"
+    shift
+    grow
+    fixed
+  >
+    <v-btn to="/">
+      <span>Dashboard</span>
+
+      <v-icon>mdi-monitor-dashboard</v-icon>
+    </v-btn>
+
+    <v-btn to="/database">
+      <span>Database</span>
+
+      <v-icon>mdi-server</v-icon>
+    </v-btn>
+
+    <v-btn to="/storage">
+      <span>Storage</span>
+
+      <v-icon>mdi-folder-outline</v-icon>
+    </v-btn>
+
+    <v-btn to="/users">
+      <span>Users</span>
+
+      <v-icon>mdi-account-group-outline</v-icon>
+    </v-btn>
+
+
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+    name: 'SiteBottomNavigation',
+    data () {
+        return {
+            isMobile: window.Leaf.mobile
+        }
+         
+         }
+  }
+</script>
+
+<style>
+</style>
--- app/assets/javascripts/leaf/layouts/ApplicationLayout.vue
+++ app/assets/javascripts/leaf/layouts/ApplicationLayout.vue
@@ -1,16 +1,21 @@
 <template>
-<v-main id="leaf-mountpoint">
-  <SiteHeaderComponent />
-  <router-view />
-</v-main>
+  <div>
+    <v-main id="leaf-mountpoint">
+      <SiteHeaderComponent />
+      <router-view />
+    </v-main>
+    <SiteBottomNavigation />
+  </div>
 </template>
 
 <script>
 import SiteHeaderComponent from '../components/site-header'
+import SiteBottomNavigation from '../components/site-bottom-navigation'
 
 export default {
   components: {
-    SiteHeaderComponent
+    SiteHeaderComponent,
+    SiteBottomNavigation
 },
 }
 </script>
